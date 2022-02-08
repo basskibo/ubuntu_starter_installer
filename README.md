5994628486e49508645d00a3afbe630d992c1d39
Bojan Jagetic <bjagetic@rtrkn880-lin.domain.local>
2022-02-09 00:10:23 +0100

    zsh and gnome added

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# ubuntu_starter_essentials
+# Ubuntu starter essentials for 20.04 (Focal Fossa)
+
+# About
+
+This is basic script which I use to make my life easier when setuping fresh Ubuntu. Essentials for my consist of:
+
+-  Visual studio code
+-  Chrome
+-  Skype
+-  htop
+-  terminator
+-  zsh
+-  ohmyzsh
+-  chrome gnome-shell
+-  gnome tweaks
+-  Spotify
+-  Git
+-  Docker
+-  Albert
+-  npm
+-  n
--- ubuntu_20.04_initial_setup.sh
+++ ubuntu_20.04_initial_setup.sh
@@ -5,6 +5,16 @@ NC='\033[0m' # No Color
 
 sudo snap install skype
 sudo snap install spotify
+curl https://build.opensuse.org/projects/home:manuelschneid3r/public_key | sudo apt-key add -
+echo 'deb http://download.opensuse.org/repositories/home:/manuelschneid3r/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:manuelschneid3r.list
+sudo wget -nv https://download.opensuse.org/repositories/home:manuelschneid3r/xUbuntu_21.10/Release.key -O "/etc/apt/trusted.gpg.d/home:manuelschneid3r.asc"
+sudo apt update
+sudo apt install albert
+# zsh and ohmyzsh
+sudo apt install zsh -y
+ zsh --version
+ cd ~
+ sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)" -y
 sudo apt-get install git -y
 sudo apt-get install npm -y
 sudo npm i n -g
@@ -36,3 +46,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 #sudo docker run hello-world
 sudo chmod 666 /var/run/docker.sock
+
+# GNome shell and tweaks
+sudo apt-get install chrome-gnome-shell
+sudo apt install gnome-tweaks
